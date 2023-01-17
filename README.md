25e9d5b9fee7aa92f8a87214534d0c1efec6b45b
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-01-17 17:17:21 +0000

    complete my simple react todo list

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
+
+
+## imeges
+![todo list empty](public/screenshot_20230117_171407.png)
+
+![after adding some taskes](public/screenshot_20230117_171324.png)
--- src/App.js
+++ src/App.js
@@ -1,9 +1,86 @@
+import React, {useState} from 'react';
+
+
 import './App.css';
 
 function App() {
+
+  //state hook useState
+  const [newItem, setNewItem]=useState("");
+  const [items,setItems]=useState([]);
+
+  const addItem= ()=>{
+    if(!newItem){
+      alert("Please enter an item and try again")
+      return;
+    }
+    console.log(newItem);
+    const item={
+      id:Math.floor(Math.random()*1000),
+      value:newItem
+    }
+
+    setItems(oldList=>[...oldList,item])
+    setNewItem("");
+  };
+
+  const deletItem =(id)=>{
+    const newArray=items.filter((item) => item.id !==id  );
+    setItems(newArray);
+  }
+
+  const UpdateItem =(id)=>{
+    if (!newItem) {
+      alert("Please enter an item and try again")
+      return;
+    }
+    const newArray=items.map((item) => {
+      if(item.id ===id ){
+        item.value=newItem;     
+      }
+      return item;
+  });
+    setItems(newArray);
+    setNewItem("");
+  }
+
   return (
     <div className="App">
-    <h1>Start React App</h1>
+      {/* header */}
+      <div className='wraper'>
+
+
+        <h1>Todo list</h1>
+
+
+
+
+        {/* input and button */}
+        <input
+          type="text"
+          placeholder='type new task ...'
+          value={newItem}
+          onChange={(e)=>setNewItem(e.target.value)}
+        />
+        <button className='btnAdd' onClick={() => addItem()}>add</button>
+
+
+
+
+        {/* unorderd list of taskes */}
+        <ul>
+         {
+            items.map(item => {
+              
+              return (
+                <li key={item.id}>{item.value} <button className='btnDelet' onClick={() => deletItem(item.id)}>Delete</button><button className='btnUpdate' onClick={() => UpdateItem(item.id)}>Update  With input text</button> </li>
+              )
+            }
+            )
+         }    
+        </ul>
+
+      </div>
     </div>
   );
 }
